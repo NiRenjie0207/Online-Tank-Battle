d3ec79d54323102475cefe0e274ab8fbdc93450c
Renjie1999 <Renjie.Ni@traderev.com>
2024-07-15 23:31:02 -0400

    forgot about commit Group and ResourceMgr before.

---
+++ src/com/nrj/tank/Group.java
@@ -0,0 +1,7 @@
+package com.nrj.tank;
+
+public enum Group {
+    GOOD, BAD
+}
+
+
---
+++ src/com/nrj/tank/ResourceMgr.java
@@ -0,0 +1,26 @@
+package com.nrj.tank;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class ResourceMgr {
+    public static BufferedImage tankL, tankU, tankR, tankD;
+    public static BufferedImage bulletL, bulletU, bulletR, bulletD;
+
+    static {
+        try {
+            tankL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankL.gif"));
+            tankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
+            tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
+            tankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
+            bulletL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletL.gif"));
+            bulletR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletR.gif"));
+            bulletU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletU.gif"));
+            bulletD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
